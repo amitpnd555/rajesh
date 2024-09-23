99cde740e489a969a93386021600b2b35ecc7d1b
Nilesh09Deshmukh <nilesh@orangeleague.in>
2024-09-23 19:03:13 +0530

    title changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -63,8 +63,9 @@ const Header = ({ handleSelect = null }) => {
       
         <div className="mb-2 md:mb-0 sm:mx-5  mr-2">
           <Typography variant="h5" className="text-white" sx={{  cursor: "pointer", fontWeight: "bold" }} onClick={() => navigate("/")}>
-            MINISTER Home
+            MINISTER Home Master Branch 
           </Typography>
+          
         </div>
         
 
